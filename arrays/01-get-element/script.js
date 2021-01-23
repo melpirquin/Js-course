7890a01a6e7a9b9f8cc83642a1dab6da7fbe8f78
Mel Pirquin <76951675+melpirquin@users.noreply.github.com>
2021-01-23 19:27:07 +0100

    arrays

---
+++ arrays/01-get-element/script.js
@@ -0,0 +1,26 @@
+/* becode/javascript
+ *
+ * /05-arrays/01-get-element/script.js - 5.1: élément de tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+
+(function() {
+
+        document.getElementById("run").addEventListener("click", function() {
+
+            let fruits = ["apple", "pear", "raspberry", "tomatoes", "kiwi", "banana", "orange", "mandarin", "durian", "peach", "grape", "cherry",];
+            let a = fruits[3];
+            alert(a);
+        })
+
+    }
+
+)();
+
+
---
+++ arrays/09-rand-array-stats/script.js
@@ -0,0 +1,39 @@
+/* becode/javascript
+ *
+ * /05-arrays/09-rand-array-stats/script.js - 5.9: tableau aléatoire & statistiques
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+
+    // Creating empty array to overwrite
+
+  /*  let lottery = [0,0,0,0,0,0,0,0,0,0];
+
+    document.getElementById("run").addEventListener("click", function () {
+
+        // Generating random numbers, overwriting lottery values, adding to sum
+
+        for (let i = 0; i < lottery.length; i++) {
+
+            let random = Math.floor(Math.random() * 100 + 1);
+            lottery[i] = random;
+            document.getElementById('n-' + (i+1)).innerText = random;
+        }
+
+        // Posting stats
+
+        document.getElementById("min").innerText = Math.min.apply( null, lottery);
+        document.getElementById("max").innerText = Math.max.apply(null, lottery);
+        document.getElementById("sum").innerText = lottery.reduce(function(a,b){return a + b},0);
+        document.getElementById("average").innerText = lottery.reduce(function (a,b) {return (a + b)},0)/10;
+
+    })*/
+
+
+})();
